02f099f7262aded707b9f7936e1b1e2c1ba410d6
Marcin Wosinek <marcin.wosinek@gmail.com>
2024-12-20 23:52:53 +0100

    Add interface for settings page

--- webapp/src/app/settings/settings.page.ts
+++ webapp/src/app/settings/settings.page.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 import { TariffService } from '../data/tariff.service';
+import { Tariff } from 'core';
 
 @Component({
   selector: 'app-settings.page',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    IonContent,
+    IonHeader,
+    IonInput,
+    IonTitle,
+    IonToolbar,
+  ],
   templateUrl: './settings.page.html',
   styleUrl: './settings.page.scss',
 })
-export class SettingsPageComponent {
+export class SettingsPageComponent implements OnInit {
   constructor(private tariffService: TariffService) {}
+
+  public tariff: Tariff | undefined;
+
+  ngOnInit() {
+    this.tariff = this.tariffService.getValue();
+  }
+
+  updateTariff() {
+    if (this.tariff) {
+      this.tariffService.setValue(this.tariff);
+    }
+  }
 }
